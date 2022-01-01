903b6c1a7d34a7ce93801589c604ce5fa5551a80
WillBurdett <88317339+WillBurdett@users.noreply.github.com>
2022-01-01 14:12:26 +0000

    Fib Product complete

--- src/com/btn/FibonacciProduct.java
+++ src/com/btn/FibonacciProduct.java
@@ -5,26 +5,49 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FibonacciProduct {
+
+    public  static long[] fibSequence(long length) {
+        List<Long> f = new ArrayList<>(List.of(0L, 1L, 1L));
+        Long num1 = 0L, num2 = 1L;
+
+        for (int i = 1; i <= length; ++i) {
+            f.add(num1);
+            Long sumOfPrevTwo = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrevTwo;
+        }
+        return f.stream().mapToLong(l -> l).toArray();
+    }
+
+    public static long[] result(Long a, long b, boolean bool){
+        return bool ? new long[]{a,b,1}:new long[]{a,b,0};
+    }
+
     public static long[] productFib(long prod) {
+        long[] fSeq = Arrays.stream(fibSequence(prod/prod * 3)).toArray();
+        for (int i = 1; i < fSeq.length; i++){
+            if (fSeq[i] * fSeq[i-1] == prod) {
+                return result(fSeq[i-1], fSeq[i],true);
+            } else if (fSeq[i] * fSeq[i-1] > prod){
+                return result(fSeq[i-1], fSeq[i], false);
+            }
+        }
         return null;
     }
-    public  static long[] fibSequence(long length){
-        // create Fibonacci Sequence
-        List<Long> f  = new ArrayList<>(List.of(0L, 1L, 1L));
-        Long sum = 0L;
-        for (int j =0; j < length - 3; j++) {
-            for (int i = f.size()-1; i > f.size() - 3; i--) {
-                sum += f.get(i);
-            }
-            f.add(sum);
-            sum = 0L;
+    // user-submitted
+    public static long[] userProductFib(long prod) {
+        long a = 0L;
+        long b = 1L;
+        while (a * b < prod) {
+            long tmp = a;
+            a = b;
+            b = tmp + b;
         }
-        long[] result = f.stream().mapToLong(l -> l).toArray();
-        System.out.println(Arrays.toString(result));
-        return result;
+        return new long[] { a, b, a * b == prod ? 1 : 0 };
     }
 
     public static void main(String[] args) {
-        System.out.println(fibSequence(5L));
+        System.out.println(fibSequence(100L));
+        System.out.println(productFib(58L));
     }
 }
