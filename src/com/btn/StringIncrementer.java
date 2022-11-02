53c852cf0ba3574aa3d522a3eb27d826be170db4
WillBurdett <williamburdett1995@gmail.com>
2022-11-02 17:26:28 +0000

    string incrementer refactor plus additional solution

--- src/com/btn/StringIncrementer.java
+++ src/com/btn/StringIncrementer.java
@@ -1,57 +1,58 @@
 package com.btn;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringIncrementer {
 
+    // my solution
     public static String incrementString(String str) {
 
-        String endOfString = "";
         String startOfString = "";
         String numbersToIncrement = "";
-        boolean hasBeenNumbers = false;
+        boolean startedWithLettersOrHasHadANonNine = false;
 
         String[] inputArr = str.split("");
 
         for (int i = inputArr.length-1; i >= 0; i--) {
 
-            if (!isNumber(inputArr[i]) && !numbersToIncrement.equals("")){
+            if (!isNumber(inputArr[i])){
                 startOfString = inputArr[i] + startOfString;
-                hasBeenNumbers = true;
-            } else if (!isNumber(inputArr[i]) && numbersToIncrement.equals("")) {
-                endOfString = inputArr[i] + endOfString;
-            } else if (isNumber(inputArr[i]) && !hasBeenNumbers && !inputArr[i].equals("9")) {
+                startedWithLettersOrHasHadANonNine = true;
+            } else if (isNumber(inputArr[i]) && !startedWithLettersOrHasHadANonNine && !inputArr[i].equals("9")) {
                 numbersToIncrement = inputArr[i] + numbersToIncrement;
-                hasBeenNumbers = true;
-            } else if (inputArr[i].equals("9") && !hasBeenNumbers) {
+                startedWithLettersOrHasHadANonNine = true;
+            } else if (inputArr[i].equals("9") && !startedWithLettersOrHasHadANonNine) {
                 numbersToIncrement = inputArr[i] + numbersToIncrement;
-            } else if (hasBeenNumbers){
+            } else if (startedWithLettersOrHasHadANonNine){
                 startOfString = inputArr[i] + startOfString;
             }
         }
-        if (numbersToIncrement.equals("") || !isNumber(inputArr[inputArr.length-1])){
-            return str + "1";
-        }
-
-        return  startOfString + formatNumbers(numbersToIncrement) + endOfString;
-
+        return numbersToIncrement.equals("") ? str + "1" :
+        startOfString + formatNumbers(numbersToIncrement) ;
     }
 
     public static String formatNumbers(String s){
-        String incrementedNumber = String.valueOf(Long.valueOf(s) + 1);
-        while (incrementedNumber.length() < s.length()){
-            incrementedNumber = "0" + incrementedNumber;
+        String numToIncrement = String.valueOf(Long.valueOf(s) + 1);
+        while (numToIncrement.length() < s.length()){
+            numToIncrement = "0" + numToIncrement;
         }
-        return incrementedNumber;
+        return numToIncrement;
     }
 
     public static Boolean isNumber(String s){
-        String nums = "0123456789";
-        String[] numArr = nums.split("");
-        for(int i=0; i < numArr.length; i++){
-            if (numArr[i].equals(s)){
-                return true;
-            }
-        }
-        return false;
+        return s.matches("\\d");
+    }
+
+    // others user's recursive solution
+    public static String incrementString2(String str) {
+
+        List<String> check = Arrays.asList(new String[] {"0","1","2","3","4","5","6","7","8","9"});
+
+        if(str.length()== 0|| !check.contains(str.substring(str.length()-1))){return str +"1";}
+
+
+        return str.substring(str.length()-1).equals("9") ? incrementString(str.substring(0,str.length()-1)) +"0": str.substring(0,str.length()-1) + String.valueOf( Integer.parseInt( str.substring(str.length()-1) ) +1  );
     }
 
     public static void main(String[] args) {
